c65d83f507fb374884b49f2cdbd53574a856a7ff
takxz <164672018+takxz@users.noreply.github.com>
2025-05-12 11:12:20 +0200

    Filtrage par mot clé + enregistrement dans un fichier externe

--- notebook_scraper.py
+++ notebook_scraper.py
@@ -3,16 +3,46 @@ import feedparser
 with open('rss_list.txt', 'r', encoding='utf-8') as f:
     rss_urls = [line.strip() for line in f if line.strip()]
 
+def load_keyword(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        return [mot.strip().lower() for mot in f if mot.strip()]
+
+
+def write_result(file, result):
+    with open(file, "w", encoding='utf-8') as f:
+        f.write(result)
+    
+result = ""
+
+mot_clef = load_keyword('mot-cle.txt')
 
 
 for url in rss_urls:
     print(f"URL du feed : {url}")
     feed = feedparser.parse(url)
     for entry in feed.entries:
-        print("Titre:", entry.get("title", "N/A"))
-        print("Publication:", entry.get("published", "N/A"))
-        print("URL:", entry.get("link", "N/A"))
-        print("\n")
+        filtre = f"{entry.get('title', '')} {entry.get('summary', '')}".lower()
+        for mot in mot_clef:
+            if mot in filtre:
+                title = entry.get("title", "N/A")
+                date = entry.get("published", "N/A")
+                url = entry.get("link", "N/A")
+                print("Titre: ", title)
+                print("Date de publication: ", date)
+                print("URL:", url)
+                print("mot clé correspondant:", mot)
+                print("\n")
+                result += f"Titre: {title} \n"
+                result += f"Date: {date}\n"
+                result += f"URL: {url}\n"
+                result += f"mot-clé correspndant: {mot}\n"
+                result += "------------------------\n"
+            
+write_result('resultat.txt', result)
+
+
+
+
 
 
     
